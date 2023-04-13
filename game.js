bf8f48729a0013afd92a397b1042da181957af3a
phillip191 <127802102+phillip191@users.noreply.github.com>
2023-04-13 17:49:20 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
 
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame)
+
+
+function startGame(){
+    //game.fillRect(0,50,100,100);
+    //game.clearRect(0,0,50,50);
+    //game.clearRect(50,50,50,50);
+    game.font = "25px Verdana"
+    game.fillStyle = "purple";
+    game.textAlign = "center";
+    game.fillText('Platzi',50,50)
+}
